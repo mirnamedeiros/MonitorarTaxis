4853feba69af8790e3c2f0976540c651ea1245e1
Mirna Medeiros <mirnamedeiros@pop-os.localdomain>
2022-07-18 17:46:54 -0300

    ---

---
+++ src/main/java/ufrn/mirna/MonitorarTaxis/MonitorarTaxisApplication.java
@@ -0,0 +1,13 @@
+package ufrn.mirna.MonitorarTaxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorarTaxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorarTaxisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufrn/mirna/MonitorarTaxis/MonitorarTaxisApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.mirna.MonitorarTaxis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorarTaxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
